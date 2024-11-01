d8d39a9cc2addf84a4839ea54130177062b14692
getssh <gtamrat33@gmail.com>
2024-11-01 15:07:18 +0300

    Update signup route to post and add json parser

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,9 @@ import authRoutes from './routes/auth.routes.js'
 import connectMongoDB from './db/connectDB.js';
 
 dotenv.config();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 const app = express();
+app.use(express.json())
 
 app.use('/api/auth', authRoutes);
 
