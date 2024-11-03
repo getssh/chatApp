211c15db9b24d1d85b1244200997fd5d4b86d036
getssh <gtamrat33@gmail.com>
2024-11-03 16:09:29 +0300

    Add message model, route, and path

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.routes.js'
+import messageRoutes from './routes/message.routes.js'
 import connectMongoDB from './db/connectDB.js';
 
 dotenv.config();
@@ -9,6 +10,7 @@ const app = express();
 app.use(express.json())
 
 app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes);
 
 app.listen(PORT, () => {
   connectMongoDB();
