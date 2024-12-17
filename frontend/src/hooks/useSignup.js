0baeecf1f528907cab70d0868469e6073e01becd
getssh <gtamrat33@gmail.com>
2024-12-17 13:48:26 +0300

    Add to conditonally render pages

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
 import toast from 'react-hot-toast'
+import { useAuthContext } from '../context/AuthContext'
 
 const useSignup = () => {
   const [loading, setLoading] = useState(false)
+  const {setAuthUser} = useAuthContext()
 
   const signup = async ({fullName, username, password, confirmPassword, gender}) => {
     const success = handleErrors(fullName, username, password, confirmPassword, gender)
@@ -16,8 +18,14 @@ const useSignup = () => {
         body: JSON.stringify({fullName, username, password, confirmPassword, gender})
       })
 
-      const date = await res.json()
-      console.log(date)
+      const data = await res.json()
+      if (data.error) {
+        throw new Error(data.error)
+      }
+
+      localStorage.setItem("chat-user", JSON.stringify(data))
+      setAuthUser(data)
+
     } catch (error) {
       
     } finally {
