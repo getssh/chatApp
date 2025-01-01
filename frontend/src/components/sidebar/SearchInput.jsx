9df433ac565835caef4e0d4dda515c1a915bfb09
getssh <gtamrat33@gmail.com>
2025-01-01 17:43:32 +0300

    Add search functionlity for users

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -1,9 +1,35 @@
+import { useState } from "react";
 import { FaSearch } from "react-icons/fa";
+import useConversation from "../../zustand/useConversation";
+import useGetConversations from "../../hooks/useGetConversation"
+import toast from "react-hot-toast";
 
 const SearchInput = () => {
+  const [search, setSearch] = useState("")
+  const {setSelectedConversation} = useConversation()
+  const {conversations} = useGetConversations()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if (!search) return
+
+    const conversation = conversations.find((c) => c.fullName.toLowerCase().includes(search.toLowerCase()))
+
+    if (conversation) {
+      setSelectedConversation(conversation)
+      setSearch("")
+    } else {
+      toast.error('No matching user found!')
+    }
+  }
+
   return (
-    <form className='flex items-center gap-2'>
-      <input type='text' placeholder='Search...' className='input input-bordered roudned-full' />
+    <form className='flex items-center gap-2' onSubmit={handleSubmit}>
+      <input type='text' placeholder='Search...' className='input input-bordered roudned-full' 
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
       <button type='submit' className='btn btn-circle bg-sky-500 text-white'>
         <FaSearch />
       </button>
