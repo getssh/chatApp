a6d187b09062dc5158522d58ef245f76fd7f57b5
getssh <gtamrat33@gmail.com>
2025-01-01 15:06:58 +0300

    Update message comps to be dynamic and Add skelotpn and timeformatter

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,18 +1,29 @@
 import React from 'react'
+import { useAuthContext } from '../../context/AuthContext'
+import useConversation from '../../zustand/useConversation'
+import { extractTime } from '../../utils/extractTime'
 
-const Message = () => {
+const Message = ({message}) => {
+  const {authUser} = useAuthContext()
+  const {selectedConversation} = useConversation()
+  const fromMe = message.senderId === authUser._id
+  const chatClassName = fromMe ? 'chat-end' : 'chat-start'
+  const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic
+  const bubbleBg = fromMe ? 'bg-blue-500' : ""
+  const formatedTime = extractTime(message.createdAt)
+
   return (
-    <div className='chat chat-end'>
+    <div className={`chat ${chatClassName}`}>
       <div className='chat-image avatar'>
         <div className='w-10 rounded-full'>
           <img
             alt='Chat bubble'
-            src='https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp'
+            src={profilePic}
           />
         </div>
       </div>
-      <div className={`chat-bubble text-white bg-blue-500`}>Hi! What's up?</div>
-      <div className='chat-footer opacity-50 text-xs text-white flex gap-1 items-center'>09:45</div>
+      <div className={`chat-bubble text-white ${bubbleBg} pb-1`}>{message.message}</div>
+      <div className='chat-footer opacity-50 text-xs text-white flex gap-1 items-center'>{formatedTime}</div>
     </div>
   )
 }
