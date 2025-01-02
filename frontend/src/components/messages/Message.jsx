65598c91317efa8895bc6592c47b09f7e79f2b2f
getssh <gtamrat33@gmail.com>
2025-01-02 15:45:25 +0300

    Add new message noificaiton sound and animation

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -11,6 +11,7 @@ const Message = ({message}) => {
   const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic
   const bubbleBg = fromMe ? 'bg-blue-500' : ""
   const formatedTime = extractTime(message.createdAt)
+  const shakeClass = message.shouldShake ? "shake" : ""
 
   return (
     <div className={`chat ${chatClassName}`}>
@@ -22,7 +23,7 @@ const Message = ({message}) => {
           />
         </div>
       </div>
-      <div className={`chat-bubble text-white ${bubbleBg} pb-1`}>{message.message}</div>
+      <div className={`chat-bubble text-white ${bubbleBg} ${shakeClass} pb-1`}>{message.message}</div>
       <div className='chat-footer opacity-50 text-xs text-white flex gap-1 items-center'>{formatedTime}</div>
     </div>
   )
