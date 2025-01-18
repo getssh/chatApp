1af1503561ce24def2c24e9c9e9e2285476675c4
getssh <gtamrat33@gmail.com>
2025-01-18 15:28:19 +0300

    Forgot importing daisy not done

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+
 export default {
   content: [
     "./index.html",
@@ -8,4 +10,4 @@ export default {
     extend: {},
   },
   plugins: [daisyui],
-}
+};
