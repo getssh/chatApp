1ae5c08e4d122369edffd7e6354133ce22416ab6
getssh <gtamrat33@gmail.com>
2025-01-18 13:54:18 +0300

    Add command to build and start app from server

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,9 @@ import authRoutes from './routes/auth.routes.js'
 import messageRoutes from './routes/message.routes.js'
 import connectMongoDB from './db/connectDB.js';
 import userRoutes from './routes/user.routes.js'
+import path from 'path'
+
+const __dirname = path.resolve()
 
 dotenv.config();
 const PORT = 3000;
@@ -16,6 +19,12 @@ app.use(cookieParser())
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 
 server.listen(PORT, () => {
   connectMongoDB();
